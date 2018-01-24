671caf78bf8421e02475758986126d5100ed3d41
Brandon Hussey <brandon.hussey048@gmail.com>
2018-01-23 23:39:52 -0400

    Added text input and button

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submitbtn;
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitbtn = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText2);
+        textView = (TextView) findViewById(R.id.textView);
+
+        submitbtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v)  {
+                String strInput;
+                strInput = editText.getText().toString();
+                textView.setText(strInput);
+
+            }
+
+        });
     }
 }
